d8a4907b548e9e5ff93edac7502dd17afa849c1a
Graeme Porteous <graeme@rgbp.co.uk>
2018-03-28 17:45:22 +0100

    Find current request for suggestions controller
    Need current FOI request so we can pass the ID through to the next
    controller.

--- app/controllers/foi/suggestions_controller.rb
+++ app/controllers/foi/suggestions_controller.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
 class Foi::SuggestionsController < ApplicationController
+  include FindableFoiRequest
+
   def index; end
 end
--- spec/controllers/foi/suggestions_controller_spec.rb
+++ spec/controllers/foi/suggestions_controller_spec.rb
@@ -3,6 +3,12 @@
 require 'rails_helper'
 
 RSpec.describe Foi::SuggestionsController, type: :controller do
+  let(:foi_request) { build_stubbed(:foi_request) }
+
+  before do
+    allow(FoiRequest).to receive(:find).with('1').and_return(foi_request)
+  end
+
   describe 'GET #index' do
     it 'returns http success' do
       get :index, params: { request_id: '1' }
