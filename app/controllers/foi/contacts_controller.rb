73cd41e95fd8c0f90d73d7d2ae3d0028b64cf206
Graeme Porteous <graeme@rgbp.co.uk>
2018-03-28 17:45:22 +0100

    Create contact for request
    Link HTML form to the Rails action to create contacts for the current
    FOI request.

--- app/controllers/foi/contacts_controller.rb
+++ app/controllers/foi/contacts_controller.rb
@@ -1,7 +1,29 @@
 # frozen_string_literal: true
 
 class Foi::ContactsController < ApplicationController
-  def show; end
+  include FindableFoiRequest
 
-  def update; end
+  before_action :find_or_build_contact
+
+  def show
+    @contact = @foi_request.contact || @foi_request.build_contact
+  end
+
+  def update
+    if @contact.update(contact_params)
+      redirect_to @foi_request
+    else
+      render :show
+    end
+  end
+
+  private
+
+  def find_or_build_contact
+    @contact = @foi_request.contact || @foi_request.build_contact
+  end
+
+  def contact_params
+    params.require(:contact).permit(:full_name, :email)
+  end
 end
