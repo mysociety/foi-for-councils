7add40ee2f7f8c3b2f1cfedb2afa2a3afecaee76
Graeme Porteous <graeme@rgbp.co.uk>
2018-04-11 14:52:42 +0100

    Add form builder methods for outputting errors

---
+++ app/helpers/form_builder.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+##
+# This Form Builder provides helper methods to output form validation errors
+# with correct CSS classes from GovUK elements.
+#
+# See: http://govuk-elements.herokuapp.com/form-elements/example-form-elements/
+#
+class FormBuilder < ActionView::Helpers::FormBuilder
+  def group(method, options = {}, &block)
+    raise(ArgumentError, 'require a block') unless block_given?
+    options[:class] = [options[:class]] << 'form-group-error' if error?(method)
+    @template.content_tag(:div, options, &block)
+  end
+
+  def error(method)
+    error_message = error_message_for(method)
+    return unless error_message
+    @template.content_tag(:span, error_message, class: 'error-message')
+  end
+
+  private
+
+  def error?(method)
+    @object.errors.include?(method)
+  end
+
+  def error_message_for(method)
+    return unless error?(method)
+    @object.errors.full_messages_for(method)[0]
+  end
+end
---
+++ spec/helpers/form_builder_spec.rb
@@ -0,0 +1,55 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+TestHelper = Class.new(ActionView::Base)
+
+describe FormBuilder, type: :helper do
+  let(:resource) { build(:contact, full_name: nil) }
+  let(:helper) { TestHelper.new }
+  let(:f) { FormBuilder.new(:contact, resource, helper, {}) }
+
+  before { resource.valid? }
+
+  describe '#group' do
+    subject { f.group(:full_name, class: 'myClass') {} }
+
+    context 'with resource error' do
+      it 'appends class' do
+        is_expected.to eq '<div class="myClass form-group-error"></div>'
+      end
+    end
+
+    context 'without resource error' do
+      before { resource.errors.clear }
+
+      it 'does not append class' do
+        is_expected.to eq '<div class="myClass"></div>'
+      end
+    end
+
+    context 'without block argument' do
+      subject { f.group(:full_name, class: 'myClass') }
+
+      it 'needs block argument' do
+        expect { subject }.to raise_error(ArgumentError, 'require a block')
+      end
+    end
+  end
+
+  describe '#error' do
+    subject { f.error(:full_name) }
+
+    context 'with resource error' do
+      it 'return error message span' do
+        allow(resource.errors).to receive(:full_messages_for) { ['Oops!'] }
+        is_expected.to eq '<span class="error-message">Oops!</span>'
+      end
+    end
+
+    context 'without resource error' do
+      before { resource.errors.clear }
+      it { is_expected.to be_nil }
+    end
+  end
+end
