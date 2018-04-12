4bc1d07d3f66d46111848330e70709cd0640f739
Graeme Porteous <graeme@rgbp.co.uk>
2018-04-12 17:18:33 +0100

    Specify template paths and precompiled assets

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  self.view_paths += ['gems/hackney_template/source/views']
 end
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,8 +9,33 @@ Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.paths << Emoji.images_path
 # Add Yarn node_modules folder to the asset load path.
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
+Rails.application.config.assets.paths << Rails.root.join(
+  'gems', 'hackney_template', 'source', 'assets', 'images'
+)
+Rails.application.config.assets.paths << Rails.root.join(
+  'gems', 'hackney_template', 'source', 'assets', 'javascripts'
+)
+Rails.application.config.assets.paths << Rails.root.join(
+  'gems', 'hackney_template', 'source', 'assets', 'stylesheets'
+)
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
-# Rails.application.config.assets.precompile += %w( admin.js admin.css )
+Rails.application.config.assets.precompile += %w[
+  favicon.ico
+  hackney-template*.css
+  fonts*.css
+  hackney-template.js
+  ie.js
+  apple-touch-icon-180x180.png
+  apple-touch-icon-167x167.png
+  apple-touch-icon-152x152.png
+  apple-touch-icon.png
+  gov.uk_logotype_crown_invert.png
+  gov.uk_logotype_crown_invert_trans.png
+  gov.uk_logotype_crown.svg
+  opengraph-image.png
+  hackney-logo-green.svg
+  hackney-logo-green-trans.png
+]
