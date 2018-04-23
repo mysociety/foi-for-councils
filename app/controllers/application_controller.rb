4162f9479ff90abaf76debc799ed05b350e773f5
Graeme Porteous <graeme@rgbp.co.uk>
2018-04-23 13:32:03 +0100

    Move HTTP auth to private method
    Having this at a class level meant some specs would fail due to the ENV
    variable not being present in preceding specs.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,14 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base # :nodoc:
-  if ENV.key?('HTTP_AUTH_PASSWORD')
-    http_basic_authenticate_with name: 'hackney-foi',
-                                 password: ENV['HTTP_AUTH_PASSWORD']
+  before_action :authenticate
+
+  private
+
+  def authenticate
+    return unless ENV.key?('HTTP_AUTH_PASSWORD')
+    authenticate_or_request_with_http_basic do |name, password|
+      name == 'hackney-foi' && password == ENV['HTTP_AUTH_PASSWORD']
+    end
   end
 end
