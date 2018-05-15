87736e32c9097884c874719ca34f7e5b8a3f4e4a
Gareth Rees <gareth@garethrees.co.uk>
2018-05-15 13:58:18 +0100

    Add naive FoiSuggestion service
    * Splits the text and makes a `Set` from it
    * Selects any `CuratedLink` where the text intersects with the keywords
      of that link
    * Picks the first 3 to display
    
    This is obviosuly just a proof of concept – it would be great if we
    could do this at database level.
    
    Also need to improve the styling of the suggestions.

--- spec/services/foi_suggestion_spec.rb
+++ spec/services/foi_suggestion_spec.rb
@@ -4,8 +4,23 @@ require 'rails_helper'
 
 RSpec.describe FoiSuggestion, type: :service do
   describe '.from_text' do
-    it 'returns an empty array' do
-      expect(described_class.from_text('An FOI request…')).to be_empty
+    subject { described_class.from_text(text) }
+
+    context 'with matching text and keywords' do
+      let(:text) { 'What is the budget for housing in 2018?' }
+      before { create(:curated_link, keywords: 'housing budget') }
+
+      it 'returns the matched selections' do
+        is_expected.not_to be_empty
+      end
+    end
+
+    context 'with non-matching text and keywords' do
+      let(:text) { 'Give me information on houses' }
+
+      it 'returns an empty array' do
+        is_expected.to be_empty
+      end
     end
   end
 end
