1c7e33041fb37d715eef7a8c5343cfc6b6a5c9d4
Graeme Porteous <graeme@rgbp.co.uk>
2021-03-12 10:46:35 +0000

    Add CaseManagement::Icasework::PublishedRequest
    Add mapping between application and case management library.

--- app/models/case_management/icasework.rb
+++ app/models/case_management/icasework.rb
@@ -28,6 +28,18 @@ module CaseManagement
       SubmittedRequest.new(request)
     end
 
+    def published_requests(query_params)
+      client_params = {
+        type: 'InformationRequest',
+        from: query_params[:start_date],
+        until: query_params[:end_date]
+      }
+
+      client.
+        where(client_params).
+        map { |request| self.class::PublishedRequest.new(request) }
+    end
+
     protected
 
     attr_reader :client
